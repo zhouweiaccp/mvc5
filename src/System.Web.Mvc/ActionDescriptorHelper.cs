c93aadeda0d7c0b9bd0b0c890c1c30fc15f3d8b1
zhouweiaccp <554961776@qq.com>
2014-10-13 17:11:40 +0800

    mvc5 源码学习

---
+++ src/System.Web.Mvc/ActionDescriptorHelper.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace System.Web.Mvc
+{
+    internal static class ActionDescriptorHelper
+    {
+        public static ICollection<ActionSelector> GetSelectors(MethodInfo methodInfo)
+        {
+            ActionMethodSelectorAttribute[] attrs = (ActionMethodSelectorAttribute[])methodInfo.GetCustomAttributes(typeof(ActionMethodSelectorAttribute), inherit: true);
+            ActionSelector[] selectors = Array.ConvertAll(attrs, attr => (ActionSelector)(controllerContext => attr.IsValidForRequest(controllerContext, methodInfo)));
+            return selectors;
+        }
+
+        public static ICollection<ActionNameSelector> GetNameSelectors(MethodInfo methodInfo)
+        {
+            ActionNameSelectorAttribute[] attrs = (ActionNameSelectorAttribute[])methodInfo.GetCustomAttributes(typeof(ActionNameSelectorAttribute), inherit: true);
+            ActionNameSelector[] selectors = Array.ConvertAll(attrs, attr => (ActionNameSelector)((controllerContext, actionName) => attr.IsValidName(controllerContext, actionName, methodInfo)));
+            return selectors;
+        }
+
+        public static bool IsDefined(MemberInfo methodInfo, Type attributeType, bool inherit)
+        {
+            return methodInfo.IsDefined(attributeType, inherit);
+        }
+
+        public static object[] GetCustomAttributes(MemberInfo methodInfo, bool inherit)
+        {
+            return methodInfo.GetCustomAttributes(inherit);
+        }
+
+        public static object[] GetCustomAttributes(MemberInfo methodInfo, Type attributeType, bool inherit)
+        {
+            return methodInfo.GetCustomAttributes(attributeType, inherit);
+        }
+
+        public static ParameterDescriptor[] GetParameters(ActionDescriptor actionDescriptor, MethodInfo methodInfo, ref ParameterDescriptor[] parametersCache)
+        {
+            ParameterDescriptor[] parameters = LazilyFetchParametersCollection(actionDescriptor, methodInfo, ref parametersCache);
+
+            // need to clone array so that user modifications aren't accidentally stored
+            // 需要复制数组以防用户修改意外存储
+            return (ParameterDescriptor[])parameters.Clone();
+        }
+
+        private static ParameterDescriptor[] LazilyFetchParametersCollection(ActionDescriptor actionDescriptor, MethodInfo methodInfo, ref ParameterDescriptor[] parametersCache)
+        {
+            // Frequently called, so ensure the delegates remain static
+            // 频繁调用，因此请确保委托保持不变
+            return DescriptorUtil.LazilyFetchOrCreateDescriptors(
+                cacheLocation: ref parametersCache,
+                initializer: (CreateDescriptorState state) => state.MethodInfo.GetParameters(),
+                converter: (ParameterInfo parameterInfo, CreateDescriptorState state) => new ReflectedParameterDescriptor(parameterInfo, state.ActionDescriptor),
+                state: new CreateDescriptorState() { ActionDescriptor = actionDescriptor, MethodInfo = methodInfo });
+        }
+
+        // Used to pass generic arguments to frequently called delegates, so keep as a struct to prevent heap allocation
+        // 用来传递泛型参数经常被称为委托，所以要为结构类型，以防止堆分配
+        private struct CreateDescriptorState
+        {
+            internal ActionDescriptor ActionDescriptor;
+            internal MethodInfo MethodInfo;
+        }
+    }
+}
---
+++ src/System.Web.Mvc/ActionFilterAttribute.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+namespace System.Web.Mvc
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = false)]
+    public abstract class ActionFilterAttribute : FilterAttribute, IActionFilter, IResultFilter
+    {
+        // The OnXxx() methods are virtual rather than abstract so that a developer need override
+        // only the ones that interest him.
+        //这些onXXX（）方法是虚方法而不是抽象的，这样开发者需要重写只有那些他们关心的。
+
+        public virtual void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+        }
+
+        public virtual void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public virtual void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+        }
+
+        public virtual void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+        }
+    }
+}
